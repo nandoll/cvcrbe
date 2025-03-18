8a7ba776025bd5057f5a1e88fdcc41ecb037990b
nandoll <fantezana@outlook.com>
2025-03-18 09:31:07 -0500

    Add Modules: users auth, analytics & common

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, AnalyticsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
